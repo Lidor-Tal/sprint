92279e7c40ceae10ebd0006360873eee9a37e9d4
Lidor-Tal <118883656+Lidor-Tal@users.noreply.github.com>
2022-11-23 21:29:36 +0200

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,7 +1,20 @@
 'use strict'
 const MINES = '💣'
-var gBoard = 10
+var gBoard = 3
+
+var gCell = {
+    minesAroundCount: 0,
+    isShown: false,
+    isMine: false,
+    isMarked: true
+
+
+}
 var gMINESCOUNT = 0
+var isShawn = true
+
+
+
 function initGame() {
     gBoard = buildBoard()
     renderBoard(gBoard)
@@ -16,7 +29,8 @@ function buildBoard() {
         }
     }
     board[0][2] = MINES
-    board[7][6] = MINES
+
+    board[2][0] = MINES
     setMinesNegsCount(board)
     // console.log(board)
     return board
@@ -31,7 +45,8 @@ function renderBoard(mat) {
             const cell = mat[i][j]
             const className = `cell cell-${i}-${j}`
 
-            strHTML += `<td data-i="${i}" data-j="${j}" class="${className}">${cell}</td>`
+            strHTML += `<td onclick="cellClicked(this, ${i}, ${j})" data-i="${i}" data-j="${j}",${Event} class="${className}">
+            ${cell}</td>`
         }
         strHTML += '</tr>'
     }
@@ -42,21 +57,48 @@ function renderBoard(mat) {
 }
 function setMinesNegsCount(board) {
     console.log(board)
+    var newBoard = []
     for (var i = 0; i < board.length; i++) {
         var currCell = board[i]
+        newBoard.push([])
         for (var j = 0; j < board[0].length; j++) {
             currCell = board[i][j]
-            if (currCell[i + 1])
-                if (currCell === MINES) gMINESCOUNT++
+            if (currCell === MINES) {
+                newBoard[i][j] = i, j
+                gMINESCOUNT++
+                console.log(gMINESCOUNT)
+                console.log(newBoard, newBoard)
+            }
+
         }
     }
-    console.log(gMINESCOUNT)
+}
+function numOfMinesArounMe(board, rowIdx, colIdx) {
+    var count = 0
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i >= gBoard.length) continue
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (i === rowIdx && j === colIdx) continue
+            if (j < 0 || j >= gBoard[0].length) continue
+            var cell = gBoard[i][j]
+            if (cell === MINES) {
+                count++
+                gCell.minesAroundCount = count
+                console.log(gCell)
+            }
+        }
+    }
+    console.log(count)
 }
 
-
-
-
-
+function cellClicked(elCell, i, j) {
+    var cell = gBoard[i][j]
+    if (cell === MINES) {
+        gCell.isShown = true
+        console.log(gCell)
+    }
+    numOfMinesArounMe(elCell, i, j)
+}
 
 function renderCell(location, value) {
     // Select the elCell and set the value
